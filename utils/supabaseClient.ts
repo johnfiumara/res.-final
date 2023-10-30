7524fafa991484ac64b0436bf562de59c6753336
johnfiumara <fumarajohn8@gmail.com>
2023-10-30 11:20:24 -0400

    b

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -6,4 +6,4 @@ const SUPABASE_URL = 'https://prnyckkpngvdhmvrsacd.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBybnlja2twbmd2ZGhtdnJzYWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyNTUyMTksImV4cCI6MjAwNjgzMTIxOX0.VK7btRaCoK8e2joB4XuhbkxgiL5isZuinK3ZzcLxqG4';
 
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY,{ db: { schema: 'Public' }})
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY,{ db: { schema: 'public' }})
